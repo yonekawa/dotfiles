b893816b512f44fc0c70915d899401f6e5a78558
Kenichi Yonekawa <tcgrim@gmail.com>
2013-03-01 00:54:04 +0900

    Add dash-at-point

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -610,6 +610,13 @@
 (add-hook 'markdown-mode-hook '(lambda() (setq indent-tabs-mode nil)))
 
 ;====================================
+; Dash.app
+;====================================
+(autoload 'dash-at-point "dash-at-point"
+          "Search the word at point with Dash." t nil)
+(global-set-key "\C-cd" 'dash-at-point)
+
+;====================================
 ; flymake
 ;====================================
 (require 'flymake)
